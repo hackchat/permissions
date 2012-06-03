e9856ad9ea3d669e966e5d58cee1f9b15f408cda
tkiefhaber <tom.kiefhaber@hungrymachine.com>
2012-06-03 17:14:23 -0400

    added mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Permissions::Application.config.secret_token = '00822cb7b1761bf09b13bad6dd7b8395c15fb82ce22ff27e165e11fe34cd8ac10fd24f20cc03b8a9b7bc4a835161f28417a772e23473a26c883d16227a6921ec'
