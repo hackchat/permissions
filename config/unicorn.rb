83751fadbbea3149689db607a73e4ec82e56847d
tkiefhaber <tom.kiefhaber@hungrymachine.com>
2012-06-07 16:04:19 -0400

    configging for deployment

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-root = "/home/deployer/apps/permissions/current/public"
+root = "/home/deployer/apps/permissions/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
