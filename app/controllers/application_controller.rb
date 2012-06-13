725ade84c30ac7104f4edcb8b23c4b54a0ff7829
Chris Maddox <tyre77@gmail.com>
2012-06-13 07:20:13 -0400

    updating for focus

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,3 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
-  # before_filter :require_login
-
-  def require_login
-    unless session[:user_token] || params[:auth_token]
-      if Rails.env.production?
-        redirect_to "http://login.hackchat.in"
-      else
-        redirect_to "http://localhost:4000/"
-      end
-    end
-  end
-  
 end
