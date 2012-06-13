1f03555cd1b6cc572bbf5f9a319d5e43c6c7f55b
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-06-13 09:35:53 -0400

    we love hackers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery
   include ApplicationHelper
-  before_filter :logged_in?
+  # before_filter :logged_in?
 
-  def logged_in?
-    render status: :unauthorized, :json => false unless params[:auth_token]
-  end
+  # def logged_in?
+  #   render status: :unauthorized, :json => false unless params[:auth_token]
+  # end
 
 end
