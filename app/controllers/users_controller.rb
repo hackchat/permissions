138aca8ef5a33698d48b026a752bec1f7f6cdac3
Mike Silvis <Mike.Silvis@LivingSocial.com>
2012-06-14 08:54:00 -0400

    modyifing permissions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
 
   def show
+    puts now = Time.now
     @permissions = UserRoomPermission.find_all_by_user_token(params[:id])
     if @permissions.present?
       render status: :ok, json: @permissions
     else
       render status: :ok, :json => []
     end
+    puts Time.now - now
   end
 
 end
